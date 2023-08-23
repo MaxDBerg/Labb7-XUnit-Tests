de091a848eabdecaeb916d44b0e4fd651afd1cdc
MaxDBerg <max.dahlberg@gmail.com>
2023-08-23 06:24:33 +0200

    Added 2 tests for history and division by zero Added fixture and output functionality

---
+++ TestProject1/CalculatorTest.cs
@@ -0,0 +1,85 @@
+using Labb7_XUnit_Tests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestProject1
+{
+    public class CalculatorTest : IClassFixture<CalculatorTestFixture>
+    {
+        private readonly CalculatorTestFixture _fixture;
+
+        private readonly ITestOutputHelper _output;
+
+        public CalculatorTest(CalculatorTestFixture fixture, ITestOutputHelper output)
+        {
+            _fixture = fixture;
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData(2, 1, 1)]
+        [InlineData(5, 4, 1)]
+        public void CalcAddition_(decimal expected, decimal num1, decimal num2)
+        {
+            _output.WriteLine($"Testing addition {expected} {num1} {num2}");
+            decimal sum1 = _fixture.CalculatorInstance.Addition(num1, num2);
+
+            Assert.Equal(expected, sum1);
+        }
+        
+        [Theory]
+        [InlineData(1, 2, 1)]
+        [InlineData(1, 5, 4)]
+        public void CalcSubtraction_(decimal expected, decimal num1, decimal num2)
+        {
+            _output.WriteLine($"Testing subtraction {expected} {num1} {num2}");
+
+            decimal sum1 = _fixture.CalculatorInstance.Subtraction(num1, num2);
+
+            Assert.Equal(expected, sum1);
+        }
+        
+        [Theory]
+        [InlineData(56, 8, 7)]
+        [InlineData(21504, 256, 84)]
+        public void CalcMultiplication_(decimal expected, decimal num1, decimal num2)
+        {
+            _output.WriteLine($"Testing multiplication {expected} {num1} {num2}");
+
+            decimal sum1 = _fixture.CalculatorInstance.Multiplication(num1, num2);
+
+            Assert.Equal(expected, sum1);
+        }
+        
+        [Theory]
+        [InlineData(1.5, 9, 6)]
+        [InlineData(2, 6, 3)]
+        public void CalcDivision_(decimal expected, decimal num1, decimal num2)
+        {
+            _output.WriteLine($"Testing division {expected} {num1} {num2}");
+
+            decimal sum1 = _fixture.CalculatorInstance.Division(num1, num2);
+
+            Assert.Equal(expected, sum1);
+        }
+
+        [Fact]
+        public void TestHistory()
+        {
+            _fixture.CalculatorInstance.Addition(5, 3);
+            _fixture.CalculatorInstance.Subtraction(8, 3);
+
+            var history = _fixture.CalculatorInstance.GetCalculationHistory();
+            Assert.Equal(2, history.Count);
+            Assert.Equal("5 + 3 = 8", history[0]);
+            Assert.Equal("8 - 3 = 5", history[1]);
+        }
+
+        [Fact]
+        public void TestDivisionByZero()
+        {
+            Assert.Throws<ArgumentException>(() => _fixture.CalculatorInstance.Division(10, 0));
+        }
+
+    }
+}
---
+++ TestProject1/CalculatorTestFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Labb7_XUnit_Tests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestProject1
+{
+
+    public class CalculatorTestFixture : IDisposable
+    {
+        public Calculator CalculatorInstance { get; private set; }
+
+        public CalculatorTestFixture()
+        {
+            this.CalculatorInstance = new Calculator();
+            // Perform any additional setup if needed
+        }
+
+        public void Dispose()
+        {
+            // Perform cleanup logic, if needed
+        }
+    }
+
+}
