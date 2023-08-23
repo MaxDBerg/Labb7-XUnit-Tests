a69ab36258542c4bf39c15749a4280b7a6d085ae
MaxDBerg <max.dahlberg@gmail.com>
2023-08-23 08:03:04 +0200

    Changed parameter names to be more unified

--- Labb7-XUnit-Tests/Calculator.cs
+++ Labb7-XUnit-Tests/Calculator.cs
@@ -15,34 +15,34 @@ namespace Labb7_XUnit_Tests
             calculationHistory = new List<string>();
         }
 
-        public decimal Addition(decimal value1, decimal value2)
+        public decimal Addition(decimal num1, decimal num2)
         {
-            decimal result = value1 + value2;
-            StoreCalculation(value1, value2, "+", result);
+            decimal result = num1 + num2;
+            StoreCalculation(num1, num2, "+", result);
             return result;
         }
 
-        public decimal Subtraction(decimal value1, decimal value2)
+        public decimal Subtraction(decimal num1, decimal num2)
         {
-            decimal result = value1 - value2;
-            StoreCalculation(value1, value2, "-", result);
+            decimal result = num1 - num2;
+            StoreCalculation(num1, num2, "-", result);
             return result;
         }
 
-        public decimal Multiplication(decimal value1, decimal value2)
+        public decimal Multiplication(decimal num1, decimal num2)
         {
-            decimal result = value1 * value2;
-            StoreCalculation(value1, value2, "*", result);
+            decimal result = num1 * num2;
+            StoreCalculation(num1, num2, "*", result);
             return result;
         }
 
-        public decimal Division(decimal value1, decimal value2)
+        public decimal Division(decimal num1, decimal num2)
         {
-            if (value2 == 0)
+            if (num2 == 0)
                 throw new ArgumentException("Cannot divide by zero.");
 
-            decimal result = value1 / value2;
-            StoreCalculation(value1, value2, "/", result);
+            decimal result = num1 / num2;
+            StoreCalculation(num1, num2, "/", result);
             return result;
         }
 
@@ -51,9 +51,9 @@ namespace Labb7_XUnit_Tests
             return calculationHistory;
         }
 
-        private void StoreCalculation(decimal value1, decimal value2, string operation, decimal result)
+        private void StoreCalculation(decimal num1, decimal num2, string operation, decimal result)
         {
-            string calculationString = $"{value1} {operation} {value2} = {result}";
+            string calculationString = $"{num1} {operation} {num2} = {result}";
             calculationHistory.Add(calculationString);
         }
     }
